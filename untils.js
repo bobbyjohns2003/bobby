fa870993d26334dd1e864190f6de2153e501db57
bobbyjohns2003 <alexksys2003@mail.ru>
2025-04-14 16:32:47 +0300

    Update untils.js

--- untils.js
+++ untils.js
@@ -5,7 +5,7 @@ export function validateEquation(equation) {
   // Проверка наличия оператора
   if (!operators.some(op => equation.includes(op))) return 'не хватает одного или нескольких операндов';
 
-  // Извлекаем первую операцию и проверяем наличие операндов
+  // Извлекаем первую операцию и проверяем наличие операндов, здесь я сомневаюсь в коде
   let opIndex = Math.max(
     equation.indexOf('+'),
     equation.indexOf('-'),
@@ -13,11 +13,11 @@ export function validateEquation(equation) {
     equation.indexOf('*')
   );
 
-  // Определяем левую и правую части уравнения
+  // Определяем левую и правую части уравнения, блин, синтаксис в другом коденадо было глянуть, но времени нет куча заданий
   const leftOperand = equation.slice(0, opIndex);
   const rightOperand = equation.slice(opIndex + 1);
 
-  // Проверка наличия обоих операндов
+  // Скопировал из примера
   if (!leftOperand || !rightOperand) return 'не хватает одного или нескольких операндов';
 
   // Проверка, что оба операнда являются числами
